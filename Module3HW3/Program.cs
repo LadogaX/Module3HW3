2df93a640d0003d40a6cf508bfec156bb4fd5298
Artem <LadogaX@gmail.com>
2021-12-11 23:40:28 +0200

    first load

--- Module3HW3/Program.cs
+++ Module3HW3/Program.cs
@@ -2,11 +2,53 @@
 
 namespace Module3HW3
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public void Show(bool result)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Result is: {result}");
+        }
+
+        public static void Main()
+        {
+            var program = new Program();
+
+            var class1 = new Class1();
+            class1.ShowHandler = program.Show;
+
+            var class2 = new Class2();
+
+            Func<double, double, double> powHandler = class1.Pow;
+            Predicate<double> predicate = class2.Calc(powHandler, 4.0, 5.0);
+
+            class1.ShowHandler(predicate.Invoke(3));
+        }
+
+        public class Class1
+        {
+            public Action<bool> ShowHandler { get; set; }
+
+            public double Pow(double x, double y)
+            {
+                return x * y;
+            }
+        }
+
+        public class Class2
+        {
+            public static double CalcResult { get; private set; }
+            public Predicate<double> Calc(Func<double, double, double> func, double x, double y)
+            {
+                CalcResult = func.Invoke(x, y);
+                Predicate<double> resultHandler = Result;
+
+                return resultHandler;
+            }
+
+            public bool Result(double d)
+            {
+                return CalcResult % d == 0.0;
+            }
         }
     }
 }
